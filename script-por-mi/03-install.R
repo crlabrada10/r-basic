7ae2075f95cc704489f8278517b3732642cc51f8
Carlos R. Labrada <crlabrada10@gmail.com>
2021-11-20 01:18:29 -0300

    primer comit

---
+++ script-por-mi/03-install.R
@@ -0,0 +1,13 @@
+#Comando para instalar librerías en R-Studio
+install.packages("tidyverse", dependencies = TRUE)
+
+#Comando para habilitar la librería
+library(tidyverse)
+
+install.packages("magic", dependencies = TRUE)
+
+library(magic)
+
+magic(6)
+
+install.packages()
